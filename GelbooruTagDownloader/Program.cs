dd4da3d61310a40fb0819f2f80e4634139bf03bb
Xwilarg <xwilarg@yahoo.fr>
2018-05-23 20:43:43 +0200

    Now parse all tags

--- GelbooruTagDownloader/Program.cs
+++ GelbooruTagDownloader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Reflection;
@@ -13,6 +14,14 @@ namespace GelbooruTagDownloader
         {
             if (!Directory.Exists(folderName))
                 Directory.CreateDirectory(folderName);
+            Tuple<Dictionary<string, int>, string>[] allTags = new Tuple<Dictionary<string, int>, string>[] {
+                new Tuple<Dictionary<string, int>, string>(new Dictionary<string, int>(), "Trivia"),
+                new Tuple<Dictionary<string, int>, string>(new Dictionary<string, int>(), "Artists"),
+                null,
+                new Tuple<Dictionary<string, int>, string>(new Dictionary<string, int>(), "Copyrights"),
+                new Tuple<Dictionary<string, int>, string>(new Dictionary<string, int>(), "Characters"),
+                new Tuple<Dictionary<string, int>, string>(new Dictionary<string, int>(), "Metadatas")
+            };
             Console.WriteLine("Informations will be saved in " + Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + folderName);
             Console.Write("Images analysed: 0");
             int id = 0;
@@ -26,14 +35,29 @@ namespace GelbooruTagDownloader
                     string[] tags = html.Split(' ');
                     foreach (string s in tags)
                     {
-                        switch (GetTagId(s))
+                        if (s == "")
+                            continue;
+                        var tag = allTags[GetTagId(s)];
+                        if (tag == null)
+                            Console.WriteLine("Invalid tag: " + s + ", value: " + GetTagId(s));
+                        else
                         {
-                            case '0':
-                                if (File.Exists(folderName + "trivia.dat"))
-                                    File.AppendAllText(folderName + "trivia.dat", s + Environment.NewLine);
-                                else
-                                    File.WriteAllText(folderName + "trivia.dat", s + Environment.NewLine);
-                                break;
+                            if (tag.Item1.ContainsKey(s))
+                                tag.Item1[s]++;
+                            else
+                                tag.Item1.Add(s, 1);
+                        }
+                        foreach (var t in allTags)
+                        {
+                            if (t != null)
+                            {
+                                string finalStr = "";
+                                foreach (var key in t.Item1)
+                                {
+                                    finalStr += key.Key + " " + key.Value + Environment.NewLine;
+                                }
+                                File.WriteAllText(folderName + t.Item2 + ".dat", finalStr);
+                            }
                         }
                     }
                     id++;
@@ -42,11 +66,11 @@ namespace GelbooruTagDownloader
             }
         }
 
-        private static char GetTagId(string tag)
+        private static int GetTagId(string tag)
         {
             using (WebClient wc = new WebClient())
             {
-                return (GetElementJson("<tag type=\"",  wc.DownloadString("https://gelbooru.com/index.php?page=dapi&s=tag&q=index&name=" + tag), '"')[0]);
+                return (Convert.ToInt32(GetElementJson("<tag type=\"",  wc.DownloadString("https://gelbooru.com/index.php?page=dapi&s=tag&q=index&name=" + tag), '"')[0] - '0'));
             }
         }
 
